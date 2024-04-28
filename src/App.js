f2a4b54589f6a6267b0ea40f6ae082912f4d3d00
faatihat <tomfaah@gmail.com>
2024-04-28 17:45:31 +0100

    added geolocator project and worked on custom hooks

--- src/App.js
+++ src/App.js
@@ -102,13 +102,85 @@
 //     </div>
 //   );
 // }
-import CurrConverter from "./project5/CurrConverter";
-import CurrencyConverter from "./project5/CurrencyConverter";
+// import CurrConverter from "./project5/CurrConverter";
+// import CurrencyConverter from "./project5/CurrencyConverter";
+
+// function App() {
+//   return (
+//     <CurrConverter />
+//     // <CurrencyConverter />
+//   );
+// }
+
+// geolocation pract
+import { useState } from "react";
+// import { act } from "react-dom/test-utils";
+
+function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+  // const { lat, lng } = position;
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { isLoading, position, error, getPosition };
+}
 
 function App() {
+  const [countClicks, setCountClicks] = useState(0);
+
+  const {
+    isLoading,
+    position: { lat, lng },
+    error,
+    getPosition,
+  } = useGeolocation(handleClick);
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
   return (
-    <CurrConverter />
-    // <CurrencyConverter />
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
   );
 }
 
